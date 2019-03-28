b77ad2488cc89ba3fc6cacb6ea543d803931380a
Ingimar <ingimareys93@hotmail.com>
2019-03-28 15:51:57 +0100

    commit finall

--- javascript.js
+++ javascript.js
@@ -1,10 +1,41 @@
-// JavaScript Document
-
 const blood = document.getElementById("blood");
 const fly = document.getElementById("fly");
+const audio = document.getElementById("buzzing");
 
-fly.addEventListener("click", deadfly)
+let paused = true;
+let move;
+
+fly.addEventListener("click", hit)
 
 function deadfly() {
-    fly.style.backgroundImage = "url('./media/graphics/dead.png')"
+
+
+  fly.style.backgroundImage = "url('./media/graphics/dead.png')"
+
+}
+
+// hjkælkhghjklælk
+
+function moveFly() {
+  fly.style.backgroundImage = "";
+  fly.style.left = (Math.random() * 90) + "vw";
+  fly.style.top = (Math.random() * 90) + "vh";
+  fly.style.transform = "rotate(" + (Math.random() * 360) + "deg)";
+
+
+}
+
+
+function hit() {
+  if (paused) {
+    moveFly();
+    audio.play();
+    move = setInterval(moveFly, 1000);
+    paused = false;
+  } else {
+    audio.pause();
+    clearInterval(move);
+    paused = true;
+    deadfly();
+  }
 }
